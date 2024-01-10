38fac40d018985d67a738165bc6e75b0bd4a1bcb
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-10 21:16:45 +0400

    added component

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -19,12 +19,7 @@ const ChatHome = () => {
     }
 
     const onLastMassageByParent = (data: any) => {
-        console.log(data)
-        setLastMassage((prevState: any) => {
-            const updateState = [...prevState]
-            updateState.push(data)
-            return updateState
-        })
+        setLastMassage(data)
     }
 
     useEffect(() => {

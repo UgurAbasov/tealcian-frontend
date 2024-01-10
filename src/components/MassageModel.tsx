ab423a376424ec2846911ba6598cf8592efc59c5
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-10 21:56:44 +0400

    added component

--- src/components/MassageModel.tsx
+++ src/components/MassageModel.tsx
@@ -17,8 +17,9 @@ useEffect(() => {
 
     if(props.own === 0){
         return ( <div className=" flex flex-row justify-end items-end mt-6 mr-[10px]">
-            <h1>{props.user}</h1>
+            <div className=" bg-black w-8 h-8"></div>
             <div className=" flex flex-col justify-end items-end mt-6 mr-[10px]">
+            <h1>{props.user}</h1>
         <div className=" bg-blue-500 w-[140px] break-words rounded-xl ">
             <h1 className=" px-2 py-2">{props.massage}
             </h1>

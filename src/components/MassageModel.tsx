110c940a59fe137f41c5236d1668516e3d68cdc4
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-10 22:11:23 +0400

    added component

--- src/components/MassageModel.tsx
+++ src/components/MassageModel.tsx
@@ -29,15 +29,13 @@ useEffect(() => {
     </div>)
     } else {
         return (
-            <div className=" flex flex-row justify-start items-start mt-6 ml-[10px]">
-                                  <div className=" bg-black w-8 h-8 rounded-2xl"></div>
-                      <div className=" flex flex-col justify-end items-end mt-6 mr-[10px]">
-                      <h1>{props.user}</h1>
+            <div className=" flex flex-col justify-start items-start mt-6 ml-[10px]">
+                            <h1>{props.user}</h1>
                 <div className=" bg-white w-[140px] break-words rounded-xl">
                     <h1 className=" px-2 py-2">{props.massage}
                     </h1>
                 </div>
-                </div>
+                <div className=" bg-black w-8 h-8 rounded-2xl"></div>
                 <h1 className="mr-[10px]">{time}</h1>
             </div>
         )

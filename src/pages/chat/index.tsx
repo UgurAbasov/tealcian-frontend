3a98e27f14b3da2222eaa2cdc80f216c9ed0f804
Georgiy <sadkovdanil64@gmail.com>
2024-01-12 01:39:17 +0300

    f

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -93,7 +93,7 @@ const ChatHome = () => {
     <>
       {/* <audio autoPlay src={'../../audio/zvuk-opovesheniya-sms.mp3'}></audio> */}
       <audio controls ref={audioRef}>
-        <source src='../../audio/zvuk-opovesheniya-sms.mp3' type='mp3' />
+        <source src='../../audio/zvuk-opovesheniya-sms.mp3' type='audio/mp3' />
       </audio>
       {loading ? (
         <div className='h-screen flex'>

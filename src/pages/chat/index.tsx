7498a084a0ea646f5c13a64c60677baddd419508
Georgiy <sadkovdanil64@gmail.com>
2024-01-12 19:35:17 +0300

    f

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -71,13 +71,23 @@ const ChatHome = () => {
     let arr = [];
     socket.emit('joinToAll', { targetId: 2178 });
     socket.on('sendNotification', data => {
+      console.log(data.userId);
+      console.log(localStorage.getItem('userId'));
+      console.log(localStorage.getItem('isChannel'));
       if (localStorage.getItem('isChannel') === 'true') {
         console.log('chto to');
       } else {
         if (data.userId.toString() !== localStorage.getItem('userId')) {
-            const audio = audioRef.current;
-            audio?.play()
-            console.log(data.message);
+          alert(data.message);
+          let audioF = new Audio('./zvuk-opovesheniya-sms.mp3');
+          const audio = audioRef.current;
+          if (audio && (audio.paused || audio.ended)) {
+            setIsPlaying(true);
+            audio.play();
+          } else if (audio) {
+            audio.pause();
+            setIsPlaying(false);
+          }
         } else {
           console.log('else');
         }
@@ -88,8 +98,8 @@ const ChatHome = () => {
   return (
     <>
       {/* <audio autoPlay src={'../../audio/zvuk-opovesheniya-sms.mp3'}></audio> */}
-      <audio ref={audioRef}>
-        <source src='/fsdsdfhh.mp3' type='audio/mpeg' />
+      <audio controls>
+        <source src={'./tap-notification-180637.mp3'} type='audio/mpeg' />
       </audio>
       {loading ? (
         <div className='h-screen flex'>

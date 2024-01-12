97eaa11809a5428ec80d6b9f4ab8ce22af581484
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-12 22:34:49 +0400

    added component

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -76,7 +76,9 @@ const ChatHome = () => {
       } else {
         if (data.userId.toString() !== localStorage.getItem('userId')) {
             const audio = audioRef.current;
-            audio?.play()
+            setTimeout(() => {
+                audio?.play()
+            }, 100)
           alert(data.message);
         } else {
           console.log('else');
@@ -87,7 +89,6 @@ const ChatHome = () => {
 
   return (
     <>
-      {/* <audio autoPlay src={'../../audio/zvuk-opovesheniya-sms.mp3'}></audio> */}
       <audio ref={audioRef}>
         <source src='/tap-notification-180637.mp3' type='audio/mpeg' />
       </audio>

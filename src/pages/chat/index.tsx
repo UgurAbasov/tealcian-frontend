f7f8fc188b5662ec64008302e11bec51d87139c1
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-12 21:43:22 +0400

    added component

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -77,7 +77,7 @@ const ChatHome = () => {
         if (data.userId.toString() !== localStorage.getItem('userId')) {
             const audio = audioRef.current;
             audio?.play()
-          alert(data.message);
+          console.log(data.message);
         } else {
           console.log('else');
         }

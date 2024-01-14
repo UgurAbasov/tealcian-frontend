74f37e05d5bae124b284310a32a2e9090c6e1a3e
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-14 20:28:56 +0400

    added component

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -1,6 +1,6 @@
 const ContextMenu = (props: any) => {
     return (
-        <div id="dropdown" className="z-10 absolute bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+        <div id="dropdown" className="z-10 absolute bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"  style={{top: props.position.y, left: props.position.x}}>
         <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
           <li>
             <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>

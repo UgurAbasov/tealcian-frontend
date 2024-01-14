65598ec789d4a1eb9d8c1125a44a8f13acc144df
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-14 19:51:18 +0400

    added component

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -1,6 +1,6 @@
 const ContextMenu = (props: any) => {
     return (
-    <div className=" bg-black" style={{top: props.position.y, left: props.position.x}}>
+    <div className=" absolute z-50 w-36 h- bg-black" style={{top: props.position.y, left: props.position.x}}>
         <p>Hello</p>
         <p>Hello</p>
     </div>

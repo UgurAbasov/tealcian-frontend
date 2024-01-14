5ca2fd5bb0e073c2d5c99e6cc4e83be684f078c6
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-14 16:51:20 +0400

    added component

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -106,8 +106,7 @@ const ChatHome = () => {
                 console.log(prevState, 1)
                 const update = [...prevState]
                 const index = update.findIndex((item) => item.privateId === data.privateId)
-                console.log(index)
-                // update[index] = {privateId: update[index].privateId, state: update[index].state + 1}
+                update[index] = {privateId: update[index].privateId, state: update[index].state + 1}
                 return update
               });
         } else {

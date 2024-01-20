65e4de7ae6a921337eae8cb5f232b916f7759cbf
UgurAbasov <ugurabbasov65@gmail.com>
2024-01-20 15:41:19 +0400

    added component

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -63,7 +63,7 @@ const ChatHome = () => {
           const dbPromise = await openDB('chats', 1, {
             upgrade(db) {
               gotResult.forEach((value: any, index: any) => {
-                db.createObjectStore(`First`, { keyPath: `${index}`, autoIncrement: true })
+                db.createObjectStore(`First`, { keyPath:'id', autoIncrement: true })
               })
             }
           });
